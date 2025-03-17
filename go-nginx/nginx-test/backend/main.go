480287bc6592efd421f66c465b64387e962e5331
comradyo <acesolo@mail.ru>
2025-03-17 15:12:48 +0300

    basic domain settup

--- go-nginx/nginx-test/backend/main.go
+++ go-nginx/nginx-test/backend/main.go
@@ -5,6 +5,8 @@ import (
     "net/http"
 )
 
+const port = "8888"
+
 func route(w http.ResponseWriter, r *http.Request) {
     fmt.Println("GOT REQUEST")
     fmt.Fprintf(w, "Hello, world! 😉 \n")
@@ -14,5 +16,5 @@ func main() {
     //http.HandleFunc("/api/hello-world", route)
     fmt.Printf("Hello, World!\n")
     http.HandleFunc("/", route)
-    http.ListenAndServe(":8888", nil)
+    http.ListenAndServe(":"+port, nil)
 }
